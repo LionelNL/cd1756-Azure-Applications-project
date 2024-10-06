5d5f002f83001dcc540acf27fc0e7f959246bdbd
Lionel Lynn <lionel.naylynn@chevron.com>
2024-10-06 19:17:50 +0800

    logging updated as Warning.

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -22,10 +22,10 @@ def home():
     try:
         user = User.query.filter_by(username=current_user.username).first_or_404()
         posts = Post.query.all()
-        app.logger.info('User {} accessed the home page.'.format(current_user.username))
+        app.logger.warning('User {} accessed the home page.'.format(current_user.username))
 
     except Exception as e:
-        app.logger.error('Error occurred: {}'.format(e))
+        app.logger.warning('Error occurred: {}'.format(e))
 
     return render_template(
     'index.html',
